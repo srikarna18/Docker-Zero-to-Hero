01cbf101d15e4d8fe77b9c3f6d84d65af739fb7a
srikarna18 <141703505+srikarna18@users.noreply.github.com>
2024-04-22 14:40:30 +0530

    Update urls.py

--- examples/python-web-app/devops/devops/urls.py
+++ examples/python-web-app/devops/devops/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path  # Add re_path for regex URL patterns
 
 urlpatterns = [
-    path('demo/', include('demo.urls')),
+    # Include the demo application's URL configuration
+    re_path(r'^demo/', include('demo.urls')),
+
+    # Include the admin application's URL configuration
     path('admin/', admin.site.urls),
 ]
